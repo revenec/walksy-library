a02f3dea175f2b95d053c52f690cad0214aa69fe
Mikel Vives <miquelvivesfont@hotmail.es>
2025-03-06 18:55:31 +0000

    confiugure prettier

--- .sfdx/typings/lwc/apex/BatchTrackerController.d.ts
+++ .sfdx/typings/lwc/apex/BatchTrackerController.d.ts
@@ -1,3 +1,6 @@
 declare module "@salesforce/apex/BatchTrackerController.getBatchesInSystem" {
   export default function getBatchesInSystem(): Promise<any>;
 }
+declare module "@salesforce/apex/BatchTrackerController.getBatchTrackersList" {
+  export default function getBatchTrackersList(): Promise<any>;
+}
--- force-app/main/default/lwc/wlbatchtracker/wlbatchtracker.js
+++ force-app/main/default/lwc/wlbatchtracker/wlbatchtracker.js
@@ -1,24 +1,24 @@
-import { LightningElement, wire, track } from 'lwc';
+import { LightningElement, wire, track } from "lwc";
 
 import getBatchesInSystem from "@salesforce/apex/BatchTrackerController.getBatchesInSystem";
 import getBatchTrackers from "@salesforce/apex/BatchTrackerController.getBatchTrackersList";
-import BATCH_TRACKER_OBJECT from '@salesforce/schema/Batch_Tracker__c';
-import BATCH_TRACKER_NAME from '@salesforce/schema/Batch_Tracker__c.Name';
-import BATCH_TRACKER_ACTIVE from '@salesforce/schema/Batch_Tracker__c.Active__c';
-import BATCH_TRACKER_START_DATE from '@salesforce/schema/Batch_Tracker__c.Start_Date_Field__c';
-import BATCH_TRACKER_END_DATE from '@salesforce/schema/Batch_Tracker__c.End_Date_Field__c';
-import BATCH_TRACKER_RECORD_IDENTIFIER from '@salesforce/schema/Batch_Tracker__c.Record_Identifier_Field__c';
-import { refreshApex } from '@salesforce/apex';
-import toastMessage from 'c/utils'
+import BATCH_TRACKER_OBJECT from "@salesforce/schema/Batch_Tracker__c";
+import BATCH_TRACKER_NAME from "@salesforce/schema/Batch_Tracker__c.Name";
+import BATCH_TRACKER_ACTIVE from "@salesforce/schema/Batch_Tracker__c.Active__c";
+import BATCH_TRACKER_START_DATE from "@salesforce/schema/Batch_Tracker__c.Start_Date_Field__c";
+import BATCH_TRACKER_END_DATE from "@salesforce/schema/Batch_Tracker__c.End_Date_Field__c";
+import BATCH_TRACKER_RECORD_IDENTIFIER from "@salesforce/schema/Batch_Tracker__c.Record_Identifier_Field__c";
+import { refreshApex } from "@salesforce/apex";
+import toastMessage from "c/utils";
 
 const columns = [
-    { label: 'Batch Name', fieldName: 'Name', type: 'text' },
-    { label: 'Active', fieldName: 'Active__c', type: 'text' },
-    { label: 'Start Date', fieldName: 'Start_Date_Field__c', type: 'text' },
-    { label: 'End Date', fieldName: 'End_Date_Field__c', type: 'text' },
-    { label: 'Record Identifier', fieldName: 'Record_Identifier_Field__c', type: 'text' }];
+    { label: "Batch Name", fieldName: "Name", type: "text" },
+    { label: "Active", fieldName: "Active__c", type: "text" },
+    { label: "Start Date", fieldName: "Start_Date_Field__c", type: "text" },
+    { label: "End Date", fieldName: "End_Date_Field__c", type: "text" },
+    { label: "Record Identifier", fieldName: "Record_Identifier_Field__c", type: "text" }
+];
 export default class wlbatchtracker extends LightningElement {
-    
     batchTrackerObject = BATCH_TRACKER_OBJECT;
     batchClassName = BATCH_TRACKER_NAME;
     batchStartDateField = BATCH_TRACKER_START_DATE;
@@ -32,50 +32,48 @@ export default class wlbatchtracker extends LightningElement {
     batchesInSystem = [];
 
     @wire(getBatchesInSystem)
-    wiredLoadBatches({ data,error }) {
-    // Error handling
+    wiredLoadBatches({ data, error }) {
+        // Error handling
         if (data) {
             this.batchesInSystem = data;
-        } else if(error){
-            toastMessage(this, 'error', 'Error', error.body.message);
+        } else if (error) {
+            toastMessage(this, "error", "Error", error.body.message);
         }
     }
 
     loadBatchTrackers() {
-        getBatchTrackers().then(result => {
-            this.batchTrackersList = result;
-        }).catch(error => {
-            this.error = error; 
-        });
-    };
-    
+        getBatchTrackers()
+            .then((result) => {
+                this.batchTrackersList = result;
+            })
+            .catch((error) => {
+                this.error = error;
+            });
+    }
 
     handleChange(event) {
-        console.log('test');
+        console.log("test");
         this.batchName = event.detail.value;
     }
 
     handleSubmit(event) {
         event.preventDefault();
-        try { 
+        try {
             const fields = event.detail.fields;
             fields.Name = this.batchName;
-            this.template.querySelector('lightning-record-edit-form').submit(fields);
+            this.template.querySelector("lightning-record-edit-form").submit(fields);
             this.handleReset();
             refreshApex(this.loadBatchTrackers);
         } catch (error) {
-            toastMessage(this, 'error', 'Error', error.body.message);
+            toastMessage(this, "error", "Error", error.body.message);
         }
-       
     }
 
-    handleReset() { 
+    handleReset() {
         this.batchName = null;
-        const inputFields = this.template.querySelectorAll(
-            'lightning-input-field'
-        );
+        const inputFields = this.template.querySelectorAll("lightning-input-field");
         if (inputFields) {
-            inputFields.forEach(field => {
+            inputFields.forEach((field) => {
                 field.reset();
             });
         }
